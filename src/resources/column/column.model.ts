3f9b23b007d5bb94b94efd0ec430eb487e90b816
Pavel <pl.hancharou@gmail.com>
2021-12-12 12:35:26 +0300

    fix: implement TypeScript migration to column model

--- src/resources/column/column.model.ts
+++ src/resources/column/column.model.ts
@@ -1,11 +1,19 @@
-const uuid = require('uuid');
+import { v4 as uuid } from 'uuid';
 
-class Column {
-  constructor({ id = uuid.v4(), title, order = 0 } = {}) {
-    this.id = id;
+export interface IColumn {
+  id?: string;
+  title: string;
+  order: number;
+}
+
+export class Column {
+  id: string;
+  title: string;
+  order: number;
+
+  constructor({ title, order = 0 }: IColumn) {
+    this.id = uuid();
     this.title = title;
     this.order = order;
   }
 }
-
-module.exports = { Column };
