8ad1b0de04ba6159481e03c082af5b14d2a783b4
Pavel <pl.hancharou@gmail.com>
2022-01-16 22:17:02 +0300

    fix: create User Entity

--- src/resources/users/user.model.ts
+++ src/resources/users/user.model.ts
@@ -1,40 +1,16 @@
-import { v4 as uuid } from 'uuid';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-export interface IUser {
-  id?: string;
-  name: string;
-  login: string;
-  password?: string;
-}
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
 
-export class User implements IUser {
-  id: string;
-  name: string;
-  login: string;
-  password: string;
+  @Column('text')
+  name!: string;
 
-  /**
-   * Creating an instance of a class User
-   *
-   * @param object - a first term type of IUser
-   *
-   * @returns Object type of IUser by default
-   */
-  constructor({ name = 'USER', login = 'user', password = 'P@55w0rd' }: IUser) {
-    this.id = uuid();
-    this.name = name;
-    this.login = login;
-    this.password = password;
-  }
+  @Column('text')
+  login!: string;
 
-  /**
-   * Returns the object type of IUser
-   *
-   * @param user - a first term type of IUser
-   * @returns Object type of IUser
-   */
-  static toResponse(user: IUser): IUser {
-    const { id, name, login } = user;
-    return { id, name, login };
-  }
+  @Column('text')
+  password!: string;
 }
