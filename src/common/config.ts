04ba3563f97a9926552ff2c4f79ce4669237e52c
Pavel <pl.hancharou@gmail.com>
2022-01-16 22:11:07 +0300

    fix: add postgres config

--- src/common/config.ts
+++ src/common/config.ts
@@ -3,13 +3,16 @@ import path from 'path';
 
 interface ConfigI {
   PORT: string;
-  HTTP_ADDRESS: string;
-  HTTP_ADDRESS_DEF: string;
   NODE_ENV: string;
   MONGO_CONNECTION_STRING: string;
   JWT_SECRET_KEY: string;
   AUTH_MODE: boolean;
   LOGGER_LVL: string;
+  POSTGRES_PORT: string;
+  POSTGRES_HOST: string;
+  POSTGRES_USER: string;
+  POSTGRES_PASSWORD: string;
+  POSTGRES_DB: string;
 }
 
 dotenv.config({
@@ -18,11 +21,14 @@ dotenv.config({
 
 export default <ConfigI>{
   PORT: process.env.PORT,
-  HTTP_ADDRESS: process.env.HTTP_ADDRESS,
-  HTTP_ADDRESS_DEF: process.env.HTTP_ADDRESS_DEF,
   NODE_ENV: process.env.NODE_ENV,
   MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   AUTH_MODE: process.env.AUTH_MODE === 'true',
   LOGGER_LVL: process.env.LOGGER_LVL,
+  POSTGRES_PORT: process.env.POSTGRES_PORT,
+  POSTGRES_HOST: process.env.POSTGRES_HOST,
+  POSTGRES_USER: process.env.POSTGRES_USER,
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+  POSTGRES_DB: process.env.POSTGRES_DB,
 };
