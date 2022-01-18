d372dbe3da51078f94ed865c4e1ad81c7060e1cd
Pavel <pl.hancharou@gmail.com>
2022-01-18 20:26:37 +0300

    fix: fix config file

--- src/common/config.ts
+++ src/common/config.ts
@@ -2,13 +2,13 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 interface ConfigI {
-  PORT: string;
+  PORT: number;
   NODE_ENV: string;
   MONGO_CONNECTION_STRING: string;
   JWT_SECRET_KEY: string;
   AUTH_MODE: boolean;
   LOGGER_LVL: string;
-  POSTGRES_PORT: string;
+  POSTGRES_PORT: number;
   POSTGRES_HOST: string;
   POSTGRES_USER: string;
   POSTGRES_PASSWORD: string;
@@ -19,16 +19,17 @@ dotenv.config({
   path: path.join(__dirname, '../../.env'),
 });
 
-export default <ConfigI>{
-  PORT: process.env.PORT,
-  NODE_ENV: process.env.NODE_ENV,
-  MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+export const config: ConfigI = {
+  PORT: process.env.PORT ? +process.env.PORT : 4000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  MONGO_CONNECTION_STRING:
+    process.env.MONGO_CONNECTION_STRING || 'your-mongo-db-connection-string',
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || 'secret-key',
   AUTH_MODE: process.env.AUTH_MODE === 'true',
-  LOGGER_LVL: process.env.LOGGER_LVL,
-  POSTGRES_PORT: process.env.POSTGRES_PORT,
-  POSTGRES_HOST: process.env.POSTGRES_HOST,
-  POSTGRES_USER: process.env.POSTGRES_USER,
-  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-  POSTGRES_DB: process.env.POSTGRES_DB,
+  LOGGER_LVL: process.env.LOGGER_LVL || '',
+  POSTGRES_PORT: process.env.POSTGRES_PORT ? +process.env.POSTGRES_PORT : 5432,
+  POSTGRES_HOST: process.env.POSTGRES_HOST || 'postgres',
+  POSTGRES_USER: process.env.POSTGRES_USER || 'postgres',
+  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD || 'postgres',
+  POSTGRES_DB: process.env.POSTGRES_DB || 'postgres',
 };
