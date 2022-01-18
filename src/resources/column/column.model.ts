fd008131505cdb3f1c9a9a35d6b87155ebb53ae3
Pavel <pl.hancharou@gmail.com>
2022-01-18 20:40:31 +0300

    fix: fix board column model

--- src/resources/column/column.model.ts
+++ src/resources/column/column.model.ts
@@ -1,17 +1,26 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Board } from '../boards/board.model';
 
-@Entity()
+export interface IBoardColumn {
+  id: string;
+  title: string;
+  order: number;
+}
+
+@Entity({ name: 'board_column' })
 export class BoardColumn {
   @PrimaryGeneratedColumn('uuid')
-  id!: string;
+  id?: string;
 
   @Column()
   title!: string;
 
-  @Column()
+  @Column('int')
   order!: number;
 
-  @ManyToOne(() => Board, (board) => board.columns)
-  boardId!: string;
+  @ManyToOne(() => Board, (board) => board.columns, {
+    onDelete: 'CASCADE',
+    orphanedRowAction: 'delete',
+  })
+  boardId?: string;
 }
